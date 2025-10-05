c4a9a5afc5294c0c24618b6c1c604f86fda00d1d
261246 <261246@edu.p.lodz.pl>
2025-10-05 17:21:14 +0200

    01 Zmiana na mnożenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+int Policz(int a, int b) {
+    return a*b;
+}
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
